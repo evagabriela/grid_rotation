29d56e7297db13208dff187065edd99bf068dc04
Eva Zamudio <gabriela.osu@gmail.com>
2014-02-11 16:06:53 -0800

    create simple skeleton to solve rotation

---
+++ init.rb
@@ -0,0 +1,37 @@
+# Code Challenge 
+# Input:
+  # my_nested_array = [
+                # [1,2,3],
+                # [4,5,6], 
+                # [7,8,9]
+              # ]
+
+# Output:
+  #my_nested_array.rotate = [
+                # [7,4,1]
+                # [8,5,2]
+                # [9,6,3]
+              # ]
+
+  def rotate(input)
+    rotate_grid = []
+    input.each do |nested_array|
+      # logic to go through each nested array
+    end
+    rotate_grid
+  end
+
+
+
+
+
+# Driver Code
+input = [
+      [1, 2, 3],
+      [4, 5, 6],
+      [7, 8, 9]
+    ]
+my_nested_array.rotate(input)
+
+
+
